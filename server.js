01d3fc8ece24e39f00fdced716500b431ed10ad7
Glitch (farolanf-boilerplate-project-filemetadata) <none>
2018-08-21 09:54:26 +0000

    update

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ app.get('/hello', function(req, res){
 });
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 })
 
 app.listen(process.env.PORT || 3000, function () {
