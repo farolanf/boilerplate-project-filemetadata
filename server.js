2b3b41c6e8588994d59cbcc11ee55e319aa18430
Glitch (farolanf-boilerplate-project-filemetadata) <none>
2018-08-21 09:51:03 +0000

    🗾🎐 Checkpoint ./server.js:1601241/234 ./package.json:1601241/24

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var upload = require('multer')({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -18,7 +20,9 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
-app.post('/
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
